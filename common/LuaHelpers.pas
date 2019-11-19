d6e743a2d96ad0032b376b0ae32d30a7639ed1da
toxa <untoxa@mail.ru>
2019-11-19 18:46:16 +0300

    cleanup function list on StopRegister()

--- common/LuaHelpers.pas
+++ common/LuaHelpers.pas
@@ -718,29 +718,33 @@ procedure TLuaClass.StartRegister;
 begin fStartCount:= fFuncs.Count; end;
 
 procedure TLuaClass.RegisterMethod(const AName: ansistring; AMethod: tLuaFunction);
-begin ffuncs.RegisterMethod(AName, AMethod); end;
+begin fFuncs.RegisterMethod(AName, AMethod); end;
 
 function TLuaClass.StopRegister(ALuaState: TLuaState; const ALibName: ansistring; aleave_table: boolean): integer;
 var i   : integer;
     obj : TFuncProxyObject;
 begin
   result:= 0;
-  if assigned(ALuaState) then with ffuncs do
-    if count > fStartCount then begin
-      lua_newtable(ALuaState);
-      for i:= fStartCount to count - 1 do begin
-        obj:= TFuncProxyObject(items[i]);
-        lua_pushstring(ALuaState, pAnsiChar(obj.Name));
-        PushMethod(ALuaState, obj.Method);
-        lua_settable(ALuaState, -3);
-        inc(result);
+  if assigned(ALuaState) then
+    with fFuncs do try
+      if count > fStartCount then begin
+        lua_newtable(ALuaState);
+        for i:= fStartCount to count - 1 do begin
+          obj:= TFuncProxyObject(items[i]);
+          lua_pushstring(ALuaState, pAnsiChar(obj.Name));
+          lua_pushlightuserdata(ALuaState, TMethod(obj.Method).Data);
+          lua_pushlightuserdata(ALuaState, TMethod(obj.Method).Code);
+          lua_pushcclosure(ALuaState, LuaProxyFunction, 2);
+          lua_settable(ALuaState, -3);
+          inc(result);
+        end;
+        if (length(ALibName) > 0) then begin
+          lua_pushvalue(ALuaState, -1);
+          lua_setglobal(ALuaState, pAnsiChar(ALibName));
+        end;
+        if not aleave_table then lua_pop(ALuaState, 1);
       end;
-      if (length(ALibName) > 0) then begin
-        lua_pushvalue(ALuaState, -1);
-        lua_setglobal(ALuaState, pAnsiChar(ALibName));
-      end;
-      if not aleave_table then lua_pop(ALuaState, 1);
-    end;
+    finally clear; end;
 end;
 
 procedure TLuaClass.RegisterGlobalMethod(ALuaState: TLuaState; const AName: ansistring; AMethod: tLuaFunction);
